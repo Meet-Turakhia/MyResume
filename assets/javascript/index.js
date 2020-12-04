386e724f74bb3cc189be17ac130b3ac8b4e5ce84
Meet-Turakhia <turakhiameeturesh@gmail.com>
2020-12-04 11:44:54 +0530

    adding files through commit

---
+++ assets/javascript/index.js
@@ -0,0 +1,16 @@
+function knowmore() {
+    var result = confirm("Do you want to proceed?");
+    if (result == true) {
+        var x = document.getElementById("cvLink");
+        var y = document.getElementById("activityLink");
+        if (x.style.display == "none" && y.style.display == "none") {
+            x.style.display = "inline";
+            y.style.display = "inline";
+            document.getElementById("knowmore").innerHTML = "Hide Link";
+        } else {
+            x.style.display = "none";
+            y.style.display = "none";
+            document.getElementById("knowmore").innerHTML = "Know More";
+        }
+    }
+}
